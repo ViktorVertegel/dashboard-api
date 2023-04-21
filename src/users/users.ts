d288cd27492e6e8875276a8149e9ceaa97b59f9b
ViktorVertegel <vertegel@gmail.com>
2023-04-21 18:07:30 +0300

    Добавлен бейсконтроллер

---
+++ src/users/users.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const userRouter = express.Router();
+
+userRouter.post('/login', (req, res) => {
+    res.send("Login")
+})
+
+
+userRouter.post('/register', (req, res) => {
+    res.send("Register")
+})
+
+export { userRouter };
